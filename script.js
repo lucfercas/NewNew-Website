994a0ec49dd5bc1c20362bb6661dc2050ab1b6bb
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-07-16 23:20:11 +0100

    create clock and place in container

--- script.js
+++ script.js
@@ -45,6 +45,34 @@ whySdBtn.addEventListener('click', () => {
     whySdDiv.style.display = 'block';
 });
 
+//Live clock
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+      
+    if(hh > 12){
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("clock").innerText = time; 
+    const t = setTimeout(function(){ currentTime() }, 1000); 
+  
+  }
+  
+  currentTime()
+
+
+
 //particle background
 
 const PARTICLE_QUANT = 1500;
